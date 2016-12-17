216aca744d3fd608c185592dd63c11916d744863
Rob <rdaly1490@gmail.com>
2016-12-16 23:17:54 -0600

    clean up js formatting

--- js/index.js
+++ js/index.js
@@ -1,18 +1,19 @@
-document.addEventListener('keydown', e => {
-  const audioToPlay = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const keyPressed = document.querySelector(`div[data-key="${e.keyCode}"]`);
+function playDrumSound(e) {
+	const audioToPlay = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const keyPressed = document.querySelector(`div[data-key="${e.keyCode}"]`);
 
-  if (audioToPlay) {
-  	audioToPlay.currentTime = 0; // reset audio if currently playing sound clip
-  	audioToPlay.play();
-  	keyPressed.classList.add('playing');
-  }
-});
-
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeCssTransition));
+	if (!audioToPlay) return;
+	audioToPlay.currentTime = 0; // reset audio if currently playing sound clip
+	audioToPlay.play();
+	keyPressed.classList.add('playing');
+}
 
 function removeCssTransition(e) {
 	if (e.propertyName !== 'transform') return;
 	this.classList.remove('playing');
 }
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeCssTransition));
+
+document.addEventListener('keydown', playDrumSound);
